ec881272b8695baf84c813aec1bd5ac3496662b0
martapz <marta.g.mattioli@gmail.com>
2017-08-15 17:54:41 +0100

    divide by genre, some formatting and populated contributions in index page

--- src/js/controllers/stories/index.controller.js
+++ src/js/controllers/stories/index.controller.js
@@ -7,15 +7,48 @@ StoriesIndexCtrl.$inject= ['Story', 'User'];
 function StoriesIndexCtrl(Story, User){
   const vm = this;
 
-  vm.possibleGenres = ['Adventure', 'Comedy', 'Crime', 'Romance', 'Thriller'];
-  vm.includedGenres = [];
+  vm.possibleGenres = [
+    {
+      genre: 'Adventure',
+      exists: false
+    },
+    {
+      genre: 'Comedy',
+      exists: false
+    },
+    {
+      genre: 'Crime',
+      exists: false
+    },
+    {
+      genre: 'Romance',
+      exists: false
+    },
+    {
+      genre: 'Thriller',
+      exists: false
+    }
+  ];
 
-  vm.all = Story.query();
+  Story
+    .query()
+    .$promise
+    .then(story => {
+      vm.all = story;
+    })
+    .then(() => {
+      for (var i = 0; i < vm.possibleGenres.length; i++) {
+        for (var j = 0; j < vm.all.length; j++) {
+          if (vm.possibleGenres[i].genre === vm.all[j].genre) {
+            vm.possibleGenres[i].exists = true;
+          }
+        }
+      }
+    });
 
   vm.users = User.query();
   vm.findUser = findUser;
   function findUser(e){
     vm.user = User.query({id: e});
   }
-
 }
