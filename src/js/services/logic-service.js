45427c8be4774bb1c9289ca6ff509ba620689899
olikirkness <oli@kirkness.com>
2017-08-15 22:25:19 +0100

    logic service more functional

--- src/js/services/logic-service.js
+++ src/js/services/logic-service.js
@@ -6,31 +6,61 @@ LogicService.$inject = [];
 
 function LogicService(){
   const self = this;
+  self.checkRules = function(authorContrib, rules){
+    if (authorContrib) {
+      self.containCheck = function(){
+        for (var i = 0; i < self.letterArray.length; i++) {
+          if(self.letterArray[i].toLowerCase() === rules.contain.toLowerCase()){
+            self.containLogicCheck = false;
+            return;
+          }else{
+            self.containLogicCheck = true;
+          }
+        }
+      };
 
-  self.containCheck = function(authorContribution, rules){
-    self.letterArray = authorContribution.split('');
-    for (var i = 0; i < self.letterArray.length; i++) {
-      if(self.letterArray[i].toLowerCase() === rules.contain.toLowerCase()){
-        self.containLogicCheck = false;
-        return;
+      self.startCheck = function(){
+        self.count = 0;
+        for (var a = 0; a < self.sentences.length; a++) {
+          if(self.sentences[a].split('')[0].toLowerCase() === rules.start.toLowerCase()){
+            self.count++;
+          }
+          if(self.count === self.sentences.length){
+            self.startLogicCheck = true;
+          }else{
+            self.startLogicCheck = false;
+          }
+        }
+      };
+
+      self.authorContrib = authorContrib;
+      self.rules = rules;
+
+      self.containLogicCheck = true;
+      self.startLogicCheck = true;
+
+      self.charLeft = 500 - self.authorContrib.split('').length;
+      self.sentences = self.authorContrib.split('. ');
+      self.letterArray = self.authorContrib.split('');
+
+      if(self.rules.start){
+        self.startCheck();
+      }
+      if (self.rules.contain) {
+        self.containCheck();
+      }
+      if(self.startLogicCheck && self.containLogicCheck){
+        self.submitCheck = true;
       }else{
-        self.containLogicCheck = true;
+        self.submitCheck = false;
       }
     }
   };
 
-  self.startCheck = function(sentences, rules){
-    self.count = 0;
-    for (var a = 0; a < sentences.length; a++) {
-      if(sentences[a].split('')[0].toLowerCase() === rules.start.toLowerCase()){
-        self.count++;
-      }
-      if(self.count === sentences.length){
-        self.startLogicCheck = true;
-      }else{
-        self.startLogicCheck = false;
-      }
+  self.containsInput = function(letters){
+    self.letters = letters;
+    if(self.letters.length >1){
+      self.letters = self.letters[self.letters.length - 1];
     }
   };
-
 }
