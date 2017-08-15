69cf70cc3fdc9907a1ced93eb04bd1eea274b4ae
Quiddale <qosullivan962@gmail.com>
2017-08-15 17:37:11 +0100

    first stage filtering

--- src/js/controllers/stories/index.controller.js
+++ src/js/controllers/stories/index.controller.js
@@ -2,9 +2,9 @@ angular
 .module('groupProject')
 .controller('StoriesIndexCtrl', StoriesIndexCtrl);
 
-StoriesIndexCtrl.$inject= ['Story', 'User'];
+StoriesIndexCtrl.$inject= ['Story', 'User', 'filterFilter', '$scope'];
 
-function StoriesIndexCtrl(Story, User){
+function StoriesIndexCtrl(Story, User, filterFilter, $scope){
   const vm = this;
 
   vm.possibleGenres = ['Adventure', 'Comedy', 'Crime', 'Romance', 'Thriller'];
@@ -12,10 +12,25 @@ function StoriesIndexCtrl(Story, User){
 
   vm.all = Story.query();
 
+
+  function filterStories() {
+    // vm.filtered = filterFilter(vm.all, vm.q);
+    // $scope.$watch(() => vm.q, filterStories);
+  }
+  vm.searchQuery = function(){
+    if(vm.q){
+      vm.filtering = true;
+      console.log('HELLOW');
+    }else{
+      vm.filtering = false;
+    }
+  };
+
+
   vm.users = User.query();
   vm.findUser = findUser;
   function findUser(e){
     vm.user = User.query({id: e});
   }
-
+  filterStories();
 }
